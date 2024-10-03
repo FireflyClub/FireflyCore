581baca5e4833eaad4253f54c9687eb840a18505
letheriver2007 <letheriver2007@126.com>
2024-10-03 19:50:33 +0800

    Implement uid modified

--- src/main/java/emu/lunarcore/server/packet/recv/HandlerSetClientPausedCsReq.java
+++ src/main/java/emu/lunarcore/server/packet/recv/HandlerSetClientPausedCsReq.java
@@ -9,11 +9,6 @@ import emu.lunarcore.server.packet.PacketHandler;
 import emu.lunarcore.server.packet.send.PacketClientDownloadDataScNotify;
 import emu.lunarcore.server.packet.send.PacketSetClientPausedScRsp;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.Base64;
-
 @Opcodes(CmdId.SetClientPausedCsReq)
 public class HandlerSetClientPausedCsReq extends PacketHandler {
 
@@ -24,29 +19,24 @@ public class HandlerSetClientPausedCsReq extends PacketHandler {
         session.getPlayer().setPaused(req.getIsPaused());
         session.send(new PacketSetClientPausedScRsp(session.getPlayer()));
 
-        // For uid.lua
-        String content = "";
-        try {
-            var fullpath = Paths.get(".").toAbsolutePath().normalize().resolve("config").resolve("uid.lua");
-            content = new String(Files.readAllBytes(fullpath));
-            
-        } catch (IOException e) {
-            content = new String(Base64.getDecoder().decode(""));
-
-        }
         
-        // Modified the name
+        String content = "";
         if (ConfigManager.getConfig().getAccountOptions().useModifiedUid) {
-            // Process account name
+            // Process strings
             String name = session.getPlayer().getName();
+            int uid = session.getPlayer().getUid();
             String hintText = "";
             if (session.getPlayer().getAccount().hasPermission("admin")) {
-                hintText = "<color=#FFC0CB>" + name + " ( Admin Account )" + "</color>";
+                hintText = "<color=#FFC0CB>" + name + " " + uid + " ( Admin Account )" + "</color>";
             } else {
-                hintText = name;
+                hintText = name + " " + uid;
             }
 
-            // Hardcode uid.lua extra content
+            // Hardcode content
+            content += "local function main()\nCS.UnityEngine.QualitySettings.vSyncCount = 0";
+            content += "local hint2 = CS.UnityEngine.GameObject.Find(\"/UIRoot/AboveDialog/BetaHintDialog(Clone)/Contents/VersionText\")\n" +
+                       "hint2:GetComponent(\"Text\").text = tostring(\"<color=#FFC0CB>" + ConfigManager.getConfig().gameServer.name + "</color>\")\n" +
+                       "hint2:SetActive(true)";
             content += "local hint1 = CS.UnityEngine.GameObject.Find(\"/UIRoot/AboveDialog/BetaHintDialog(Clone)/Contents/HintText\")\n" +
                           "hint1:GetComponent(\"Text\").text = tostring(\"" + hintText + "\")\n" +
                           "hint1:SetActive(true)\n";
